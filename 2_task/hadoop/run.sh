482bfe8a570b3780f4b0248b481cfd97aadb688a
Davide Galletti <d.galletti99@gmail.com>
2023-05-08 15:56:09 +0200

    Sistemato codice per il run dei task e per lo start del dfs

--- 2_task/hadoop/run.sh
+++ 2_task/hadoop/run.sh
@@ -5,19 +5,17 @@ hadoop jar $HADOOP_STREAMING_JAR_PATH \
             -mapper $TASK_DIR/mapper1.py -reducer $TASK_DIR/reducer1.py \
             -input $HDFS_DATASET_PATH -output "${HDFS_FULL_TASK_OUTPUT_DIR_PATH}_0"
 
-if [[ $? -eq 0 ]] ; then
-    echo "Task 1 completed successfully"
-    
-    echo "Starting task 2"
-    hadoop jar $HADOOP_STREAMING_JAR_PATH \
-            -mapper $TASK_DIR/mapper2.py -reducer $TASK_DIR/reducer2.py \
-            -input "${HDFS_FULL_TASK_OUTPUT_DIR_PATH}_0" -output $HDFS_FULL_TASK_OUTPUT_DIR_PATH
-    
-    hdfs dfs -rm -r $"${HDFS_FULL_TASK_OUTPUT_DIR_PATH}_0"
-
-else
+if [[ $? -gt 0 ]] ; then
     echo "Task 1 failed"
     hdfs dfs -rm -r $"${HDFS_FULL_TASK_OUTPUT_DIR_PATH}_0"
     exit 1
 fi
+
+echo "Task 1 completed successfully, starting task 2"
+hadoop jar $HADOOP_STREAMING_JAR_PATH \
+        -mapper $TASK_DIR/mapper2.py -reducer $TASK_DIR/reducer2.py \
+        -input "${HDFS_FULL_TASK_OUTPUT_DIR_PATH}_0" -output $HDFS_FULL_TASK_OUTPUT_DIR_PATH
+
+hdfs dfs -rm -r $"${HDFS_FULL_TASK_OUTPUT_DIR_PATH}_0"
+
 exit 0
