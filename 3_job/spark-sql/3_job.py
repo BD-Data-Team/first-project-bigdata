126add1dd47e7b82ab438e5025567bb6b1fce5aa
Francesco9932 <francescostefanelli98@gmail.com>
2023-05-25 12:53:22 +0200

    Renaming2 variabili 3_job spark-sql

--- 3_job/spark-sql/3_job.py
+++ 3_job/spark-sql/3_job.py
@@ -24,29 +24,30 @@ spark = SparkSession \
 args = parser.parse_args()
 input_filepath, output_filepath = args.input_path, args.output_path
 
-input_df = spark.read.csv(input_filepath, header=True,
+input_DF = spark.read.csv(input_filepath, header=True,
                           inferSchema=True).cache()
 
-productId_for_users_df = input_df.select("UserId", "ProductId").where(input_df["Score"] >= 4) \
+productId_for_users_DF = input_DF.select("UserId", "ProductId").where(input_DF["Score"] >= 4) \
     .groupBy("ProductId").agg(collect_set("UserId").alias("Users")).where(size("Users") > 3)
 
-products_for_users_df = productId_for_users_df.withColumn(
+products_for_users_DF = productId_for_users_DF.withColumn(
     "Products", array("ProductID"))
 
 for i in range(2):
-    products_for_users_df = products_for_users_df.withColumnRenamed("Users", "Users1")\
+    products_for_users_DF = products_for_users_DF.withColumnRenamed("Users", "Users1")\
         .withColumnRenamed("Products", "Products1") \
-        .crossJoin(products_for_users_df.withColumnRenamed("Users", "Users2")
+        .crossJoin(products_for_users_DF.withColumnRenamed("Users", "Users2")
                    .withColumnRenamed("Products", "Products2")) \
         .where(F.col("Products1") < F.col("Products2"))
 
-    products_for_users_df = products_for_users_df.select(array_union("Products1", "Products2").alias(
+    products_for_users_DF = products_for_users_DF.select(array_union("Products1", "Products2").alias(
         "Products"), array_intersect("Users1", "Users2").alias("Users")).distinct()
-    products_for_users_df = products_for_users_df.where(size("Products") >= 2)
+    products_for_users_DF = products_for_users_DF.where(size("Products") >= 2)
 
-# products_for_users_df = products_for_users_df.orderBy(df["Users"][0])
+output_DF = products_for_users_DF.groupBy("Products").orderBy(
+    products_for_users_DF["Users"][0])
 
-products_for_users_df = products_for_users_df.select(products_for_users_df["Products"].cast(
-    "string"), products_for_users_df["Users"].cast("string"))
-products_for_users_df.show(10)
-products_for_users_df.write.csv(output_filepath, header=True)
+output_DF = output_DF.select(output_DF["Products"].cast(
+    "string"), output_DF["Users"].cast("string"))
+# products_for_users_DF.show(10)
+output_DF.write.csv(output_filepath, header=True)
