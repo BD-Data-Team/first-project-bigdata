037dd16986367f1fbe4875f7883b130595725e3b
Francesco9932 <francescostefanelli98@gmail.com>
2023-05-25 12:25:20 +0200

    Renaming variabili 3_job spark-sql

--- 3_job/spark-sql/3_job.py
+++ 3_job/spark-sql/3_job.py
@@ -24,25 +24,29 @@ spark = SparkSession \
 args = parser.parse_args()
 input_filepath, output_filepath = args.input_path, args.output_path
 
-df = spark.read.csv(input_filepath, header=True, inferSchema=True).cache()
+input_df = spark.read.csv(input_filepath, header=True,
+                          inferSchema=True).cache()
 
-df = df.select("UserId", "ProductId").where(df["Score"] >= 4) \
-    .groupBy("ProductId").agg(collect_set("UserId").alias("Users")).having(size("Users") > 3)
+productId_for_users_df = input_df.select("UserId", "ProductId").where(input_df["Score"] >= 4) \
+    .groupBy("ProductId").agg(collect_set("UserId").alias("Users")).where(size("Users") > 3)
 
-df = df.withColumn("Products", array("ProductID"))
+products_for_users_df = productId_for_users_df.withColumn(
+    "Products", array("ProductID"))
 
 for i in range(2):
-    df = df.withColumnRenamed("Users", "Users1").withColumnRenamed("Products", "Products1") \
-        .crossJoin(df.withColumnRenamed("Users", "Users2")
-                .withColumnRenamed("Products", "Products2")) \
+    products_for_users_df = products_for_users_df.withColumnRenamed("Users", "Users1")\
+        .withColumnRenamed("Products", "Products1") \
+        .crossJoin(products_for_users_df.withColumnRenamed("Users", "Users2")
+                   .withColumnRenamed("Products", "Products2")) \
         .where(F.col("Products1") < F.col("Products2"))
 
-    df = df.select(array_union("Products1", "Products2").alias("Products"), array_intersect("Users1", "Users2").alias("Users")).distinct()
-    df = df.where(size("Products") >= 2)
+    products_for_users_df = products_for_users_df.select(array_union("Products1", "Products2").alias(
+        "Products"), array_intersect("Users1", "Users2").alias("Users")).distinct()
+    products_for_users_df = products_for_users_df.where(size("Products") >= 2)
 
-df = df.orderBy(df["Users"][0])
+# products_for_users_df = products_for_users_df.orderBy(df["Users"][0])
 
-df = df.select(df["Products"].cast("string"), df["Users"].cast("string"))
-df.show(10)
-df.write.csv(output_filepath, header=True)
-
+products_for_users_df = products_for_users_df.select(products_for_users_df["Products"].cast(
+    "string"), products_for_users_df["Users"].cast("string"))
+products_for_users_df.show(10)
+products_for_users_df.write.csv(output_filepath, header=True)
